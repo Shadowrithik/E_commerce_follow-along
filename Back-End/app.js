14fa0fd6c661c250a202e2353f06fc43c43d101a
KISHORE S <s.rithikkumarcaprio@gmail.com>
2025-02-10 09:42:58 +0530

    saved

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,23 +1,29 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const ErrorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use("/", express.static("uploads"));
+app.use("/",express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
       path: "backend/config/.env",
     });
-    require("dotenv").config({
-      path: "backend/config/.env",
-    });
 };
+// Allow CORS for Development Only
+if (process.env.NODE_ENV === 'development') {
+  const cors = require('cors');
+  app.use(cors({ origin: 'http://localhost:5173' }));
+}
+
 //import Routes
 const user = require("./controller/user");
 
